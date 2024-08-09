9d31582645c049ad24db458df36facbcbfce7ebb
Maria Eduarda <mariaeduardafbraz@outlook.com>
2024-08-09 12:42:26 -0300

    Product

---
+++ src/main/java/com/eduardafbz/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.eduardafbz.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eduardafbz.course.entities.Product;
+import com.eduardafbz.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
